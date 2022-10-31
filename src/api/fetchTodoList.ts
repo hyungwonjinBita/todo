a975f6396910f84b3fead0a399e2654a535a5ef1
hyungwonjinBita <hyungwon_jin@bita.jp>
2022-10-31 15:08:57 +0900

    chore: createAsyncThunkのtype定義

--- src/api/fetchTodoList.ts
+++ src/api/fetchTodoList.ts
@@ -1,14 +1,18 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { TodoState } from '../types';
 
-const fetchTodoData = createAsyncThunk('todo/fetch', async (_, { rejectWithValue }) => {
-  try {
-    const todoItems = await fetchApi();
-    return todoItems;
-  } catch (error) {
-    return rejectWithValue((error as Error).message);
-  }
-});
+const fetchTodoData = createAsyncThunk<TodoState, undefined, { rejectValue: string }>(
+  'todo/fetch',
+  async (_, { rejectWithValue }) => {
+    try {
+      const todoItems = await fetchApi();
+      return todoItems;
+    } catch (error) {
+      return rejectWithValue((error as Error).message);
+    }
+  },
+);
 
 const fetchApi = async () => {
   const { data } = await axios.get('https://jsonplaceholder.typicode.com/todos?userId=1');
